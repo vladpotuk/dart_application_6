06efa2e431fa485e4fa6f32e39c1a06d9ecd0c2f
vladpotuk <vladpotuk01@gmail.com>
2024-01-18 20:22:08 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+void main() {
+  print("Curriculum Vitae");
+
+  print("\nEducation:");
+  educationSection();
+
+
+  print("\nSkills:");
+  skillsSection();
+
+  print("\nLanguage:");
+  languagesSection();
+}
+
+void educationSection() {
+  print("Student, Step IT, 2023-");
+  }
+
+
+void skillsSection() {
+  print("Мови програмування: Dart, C++, Python");
+  print("Фреймворки: Flutter");
+  print("Веб-технології: HTML, CSS");
+}
+
+void languagesSection() {
+  print("Українська (рідна), Англійська (B)");
+}
